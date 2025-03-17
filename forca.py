12c92986bb6067dd71969efda1768a2b0f6490e7
gmartins9042 <gmartins9042@gmail.com>
2025-03-17 20:49:52 -0300

    Adicionei uma lista para que seja visivel para o usuario quais letras esta faltando e a ordem delas

--- forca.py
+++ forca.py
@@ -5,8 +5,11 @@ def jogar():
     print("**********************************")
 
     palavra_secreta = ("banana")
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
     enforcou = False
     acertou = False
+    print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
@@ -15,11 +18,11 @@ def jogar():
 
         index = 0
         for letra in palavra_secreta:
-            if ( chute.upper() == letra.upper() ):
-                print("Encontrei a letra {} na posição {}".format(chute, index))
+            if (chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
             index = index + 1
 
-        print("jogando...")
+        print(letras_acertadas)
 
 print("Fim do Jogo!")
 
